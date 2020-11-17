a6699c46cdd23c9029878de3c354cb25a15a7439
koressel <koressel.dev@gmail.com>
2020-11-17 11:34:02 -0600

    add bodyparser and mysql dependencies

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const bodyParser = require("body-parser");
 const path = require("path");
 const express = require("express");
 const app = express();
@@ -8,6 +9,13 @@ app.use(express.static("public"));
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.listen(5000, () => {
   console.log("server started on port 5000");
