ab02f3b526362cf7582ad7f3cd8f761e19d74b44
koressel <koressel.dev@gmail.com>
2020-11-17 12:09:07 -0600

    update PORT variable

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
@@ -18,7 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./routes/application.routes.js")(app);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
