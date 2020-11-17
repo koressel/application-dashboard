b9853b9f4387b0cd2e0eeacb18e45b00d1e6f025
koressel <koressel.dev@gmail.com>
2020-11-17 12:37:38 -0600

    add Heroku Postgres config details

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "us-cdbr-iron-east-02.cleardb.net",
-    USER: "b7e24378878xxx",
-    PASSWORD: "0200exxx",
-    DB: "heroku_7643ec736354xxx"
+    HOST: "ec2-50-19-247-157.compute-1.amazonaws.com",
+    USER: "lkijwighxtbmqk",
+    PASSWORD: "f1a36a1dd8b35b413626a85156df4598801a6b6b4f2995b274fa70b8d7bb92b8",
+    DB: "d8v9ac8ms7sbj2"
   };
