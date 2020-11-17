4cc8f48b7579221351694435b2152fcaf234169f
koressel <koressel.dev@gmail.com>
2020-11-17 12:09:47 -0600

    update PORT variable with comment

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;  //move
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
