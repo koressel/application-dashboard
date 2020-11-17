d917e83af604f3e407a66fb1115b53789a10fbe0
koressel <koressel.dev@gmail.com>
2020-11-17 11:46:08 -0600

    update PORT constant to use a ternary operator to check if environment port exists, and if not set it to port 5000

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
