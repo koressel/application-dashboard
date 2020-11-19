5890514524cfb18bcfca30cf1ea05cbd5d11c90c
koressel <koressel.dev@gmail.com>
2020-11-18 23:05:07 -0600

    add application schema

--- src/App.js
+++ src/App.js
@@ -19,7 +19,20 @@ export default class App extends React.Component {
     return (
       <div>
         <form></form>
-        <content></content>
+        <content>
+          <div>
+            <h1>Title</h1>
+            <h2>Company</h2>
+            <p>Date Applied</p>
+            <input type="checkbox">Response</input>
+            <input type="checkbox">Interview</input>
+            <input type="checkbox">Offer</input>
+            <p>Job Posting Url</p>
+            <p>Application Status Url</p>
+            <p>Notes</p>
+            <p>Resume/CV</p>
+          </div>
+        </content>
       </div>
     );
   }
