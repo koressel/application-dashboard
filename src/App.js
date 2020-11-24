962b7dd2aa378dd4a2fdd597031182922ff11b0a
koressel <koressel.dev@gmail.com>
2020-11-23 21:44:05 -0600

    populate new-application-form inputs

--- src/App.js
+++ src/App.js
@@ -19,7 +19,59 @@ export default class App extends React.Component {
     return (
       <div>
         <form id="new-application-form">
-          
+        <h1>Add new application</h1>
+        <hr/>
+        <p>
+            <label for="company">Company Name</label><br/>
+            <input name="company" type="text"/>
+        </p>
+        <p>
+            <label for="position">Position / Job Title</label><br/>
+            <input name="position" type="text"/>
+        </p>
+        <p>
+            <label for="date">Date Applied</label><br/>
+            <input name="date" type="date"/>
+        </p>
+        <p>
+            <label for="type">Shedule Type</label><br/>
+            <select name="type">
+                <option value="full-time">Full Time</option>
+                <option value="part-time">Part Time</option>
+                <option value="contract">Contract</option>
+                <option value="temporary">Temporary</option>
+                <option value="internship">Internship</option>
+            </select>
+        </p>
+
+        <fieldset>
+          <legend>Account Info</legend>
+          <p>
+            <label for="username">Username/Email</label><br/>
+            <input name="username" type="text"/>
+          </p>
+          <p>
+              <label for="password">Password</label><br/>
+              <input name="password" type="text"/>
+          </p>
+          <p>
+            <label for="status-url">Url</label><br/>
+            <input name="status-url" type="text"/>
+          </p>
+        </fieldset>
+
+        <p>
+            <label for="post-url">Job Posting Url</label><br/>
+            <input name="post-url" type="text"/>
+        </p>
+  
+        <label for="notes">Notes</label><br/>
+        <textarea name="notes" rows="10"></textarea>
+        
+        <p>
+            <button type="submit">Save</button>
+            <button type="reset">Clear</button>
+        </p>
         </form>
         <content>
           <div>
