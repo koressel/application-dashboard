eb70252483c1c054c71064ebdc6750dedf4513fc
koressel <koressel.dev@gmail.com>
2020-12-04 15:11:49 -0600

    configure postgres database connection

--- server/controllers/application.controller.js
+++ server/controllers/application.controller.js
@@ -1,14 +1,15 @@
 const Application = require("../models/application.model.js");
 
 // Retrieve all Customers from the database.
-exports.findAll = (req, res) => {
-  res.json({info: 'This is some json data.'});
-  // Application.getAll((err, data) => {
-  //   if (err)
-  //     res.status(500).send({
-  //       message:
-  //         err.message || "Some error occurred while retrieving customers."
-  //     });
-  //   else res.send(data);
-  // });
+exports.getAll = (req, res) => {
+  Application.getAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving customers."
+      });
+    else {
+      res.send(data)
+    }
+  });
 };
--- server/models/application.model.js
+++ server/models/application.model.js
@@ -1,79 +1,63 @@
 const { Pool, Client} = require("pg");
 
 const pool = new Pool({
-  user: 'lorkor',
+  user: 'server',
   host: 'localhost',
   database: 'applicationDashboard',
-  password: '',
+  password: 'server123',
   port: 5432,
 });
 
-Application.getAll = result => {
-  sql.query("SELECT * FROM application", (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(null, err);
-      return;
-    }
-
-    console.log("applications: ", res);
-    result(null, res);
-  });
+const Application = function(application) {
+  this.tile = application.title,
+  this.company = application.company
 };
 
-// Customer.updateById = (id, customer, result) => {
-//   sql.query(
-//     "UPDATE customers SET email = ?, name = ?, active = ? WHERE id = ?",
-//     [customer.email, customer.name, customer.active, id],
-//     (err, res) => {
-//       if (err) {
-//         console.log("error: ", err);
-//         result(null, err);
-//         return;
-//       }
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
 
-//       if (res.affectedRows == 0) {
-//         // not found Customer with the id
-//         result({ kind: "not_found" }, null);
-//         return;
-//       }
+Application.getAll = result => {
+  pool
+  .connect()
+  .then(client => {
+      return client
+      .query('SELECT * FROM applications')
+      .then(res => {
+        client.release();
+        result(null, res.rows);
+        console.log(res.rows);
+      })
+      .catch(err => {
+        client.release();
+        console.log(err.stack);
+        result(err, res);
+      })
+  })
 
-//       console.log("updated customer: ", { id: id, ...customer });
-//       result(null, { id: id, ...customer });
-//     }
-//   );
-// };
-
-// Customer.remove = (id, result) => {
-//   sql.query("DELETE FROM customers WHERE id = ?", id, (err, res) => {
-//     if (err) {
-//       console.log("error: ", err);
-//       result(null, err);
-//       return;
-//     }
-
-//     if (res.affectedRows == 0) {
-//       // not found Customer with the id
-//       result({ kind: "not_found" }, null);
-//       return;
-//     }
-
-//     console.log("deleted customer with id: ", id);
-//     result(null, res);
-//   });
-// };
-
-// Customer.removeAll = result => {
-//   sql.query("DELETE FROM customers", (err, res) => {
-//     if (err) {
-//       console.log("error: ", err);
-//       result(null, err);
-//       return;
-//     }
-
-//     console.log(`deleted ${res.affectedRows} customers`);
-//     result(null, res);
-//   });
-// };
+  // pool
+  //   .connect()
+  //   .then(client => {
+  //       return client
+  //       .query('$1', [1])
+  //       .then(res => {
+  //         client.release();
+  //         console.log(res.rows[0]);
+  //       })
+  //       .catch(err => {
+  //         client.release();
+  //         console.log(err.stack);
+  //       })
+  //   })
+  // pool.query('SELECT * FROM applications', (err, res) => {
+  //   if (err) {
+  //     console.log(err);
+  //     result(err);
+  //   }
+  //   result(null, res)
+  //   pool.end()
+  // })
+};
 
 module.exports = Application;
