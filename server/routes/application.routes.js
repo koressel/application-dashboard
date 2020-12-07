6165c4aaca5527065866052c8a044c3479000d6b
koressel <koressel.dev@gmail.com>
2020-12-07 11:21:04 -0600

    add root path / to routes

--- server/routes/application.routes.js
+++ server/routes/application.routes.js
@@ -2,4 +2,7 @@ module.exports = app => {
     const applications = require("../controllers/application.controller.js");
   
     app.get("/applications", applications.getAll);
+    app.get("/", (req,res) => {
+      res.render(index.html);
+    });
   };
